e96526aeaaf669c575de9d30305c5a2c4399ce17
Alfin Fernandha <alfinfernandha@adsl-172-10-8-101.dsl.sndg02.sbcglobal.net>
2022-10-24 11:36:18 +0700

    initial project

---
+++ src/main/java/com/bismillah/services/ProductService.java
@@ -0,0 +1,39 @@
+package com.bismillah.services;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bismillah.models.entities.Product;
+import com.bismillah.models.repos.ProductRepo;
+
+@Service
+@Transactional
+public class ProductService {
+    
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product save(Product product){
+        return productRepo.save(product);
+    }
+
+    public Product findOne(Long id){
+        return productRepo.findById(id).get();
+    }
+
+    public Iterable<Product> findAll(){
+        return productRepo.findAll();
+    }
+
+    public void removeOne(Long id){
+        productRepo.deleteById(id);
+    }
+
+    public List<Product> findByName(String name){
+        return productRepo.findByNameContains(name);
+    }
+}
